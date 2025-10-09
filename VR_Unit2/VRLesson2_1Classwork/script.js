af93f9db668f02f6a3ec42dd5561f30196e300e7
Myst3p1c <nathanw57@nycstudents.net>
2025-10-09 22:59:00 +0000

    Lesson1_2 homework

--- VR_Unit2/VRLesson2_1Classwork/script.js
+++ VR_Unit2/VRLesson2_1Classwork/script.js
@@ -1,15 +1,15 @@
 window.addEventListener("DOMContentLoaded",function() {
   //Challenge 1: Create a variable scene for the <a-scene> component in the HTML
-
+  let scene = document.querySelector("a-scene")
 
   //Challenge 2: Create a variable for <a-dodecahedron> and create it.
-
+  let dodecahedron = document.createElement("a-dodecahedron")
   
   //Challenge 3: Change the <a-dodecahedron>'s position, radius and color
-
+  dodecahedron.setAttribute("position",{x:0,y:2,z:0});
 
   //Challenge 4: Add the <a-dodecahedron> to the scene
-
+  scene.append(dodecahedron);
 
   /* Challenge Bonus
   1) Use a for loop to create a lot of <a-dodecahedron> 
