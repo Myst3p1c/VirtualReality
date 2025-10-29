857661e922e04abea8ba4a5699c39c45efe0f357
Myst3p1c <nathanw57@nycstudents.net>
2025-10-29 00:55:20 +0000

    launch

--- VR_Unit2/VRPractical2_4/script.js
+++ VR_Unit2/VRPractical2_4/script.js
@@ -2,22 +2,22 @@ let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene, rockets = [];
 
 window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene");
-  rocket = new Rocket(3,-2, 4);
-  // for(let i = 0; i < 100; i++){
-    //let x = rnd(-100,100);
-    //let z = rnd(-100,100);
-    //let rocket = new Rockets(x,y,z);
-    //rockets.push(rocket);
-//}
-    // loop();
+  scene = document.querySelector("a-scene"); 
+  rocket = new Rocket(3, -2 , 4);
+  for(let i = 0; i < 100; i++){
+    let x = rnd(-100,100);
+    let z = rnd(-100,100);
+    let rocket = new Rockets(x, -3, z);
+    rockets.push(rocket);
+  }
+  loop();
+})
+
+function loop(){
+  for(let rocket of rockets){
+    rocket.fly();
+  }
+  // rocket.fly();
+  
+  window.requestAnimationFrame( loop );
 }
-
-// function loop(){
-  // for(let rockets of rockets){
-  //  rocket.fly();
- // }
-
- // window.requestAnimationFrame(loop);
-
-//}
