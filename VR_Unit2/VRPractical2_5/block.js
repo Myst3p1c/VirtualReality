771bc3772db73dee1f9f192e88ffc74dde93d883
Myst3p1c <nathanw57@nycstudents.net>
2025-11-17 15:47:40 +0000

    Prac 2_5

--- VR_Unit2/VRPractical2_5/block.js
+++ VR_Unit2/VRPractical2_5/block.js
@@ -6,7 +6,15 @@ class Block{
     
     this.obj = document.createElement("a-box");
     this.obj.setAttribute("position",{x:x,y:y,z:z});
+    this.obj.setAttribute("src", "bush.jpg");
+    this.obj.setAttribute("height","2");
     scene.append(this.obj);
 
+    this.obj = document.createElement("a-sphere");
+    this.obj.setAttribute("position",{x:x,y:y,z:z});
+    this.obj.setAttribute("src", "bush.jpg");
+    this.obj.setAttribute("scale","1 2 1");
+    this.obj.setAttribute("radius","0.7");
+    scene.append(this.obj);
   }
 }
--- VR_Unit2/VRPractical2_5/script.js
+++ VR_Unit2/VRPractical2_5/script.js
@@ -1,16 +1,45 @@
 let maze = [
-  "-xxxxxxxxxxxxxxxxxxxx-------",
-  "ox-----------------xo------",
-  "-ox---------------xo-------",
-  "--ox------T------xo--------",
-  "---ox-----------xo---------",
-  "----ox---------xo-----------",
-  "-----ox-------xo-----------",
-  "------ox-----xo------------",
-  "-------ox---xo-------------",
-  "--------ox-xo--------------",
-  "---------oxo---------------",
-  "----------o----------------",
+"---xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+"---x-----------x--x--x--x-----------------x--------x--------x",
+"x--x--xxxxxxxxxx--x--x--xxxxxxxxxx--x--xxxx--x--xxxxxxxxxx--x",
+"x--------x--x--x-----x--------x-----x--x-----x-----x-----x--x",
+"xxxxxxx--x--x--x--x--x--xxxxxxxxxx--xxxx--xxxxxxxxxxxxx--x--x",
+"x-----x-----------x--x-----------x-----x--x-----------------x",
+"xxxx--xxxx--x--xxxxxxx--x--xxxxxxx--xxxx--x--x--x--xxxx--x--x",
+"x--------x--x-----------x--x-----x--x--------x--x-----x--x--x",
+"x--xxxxxxxxxxxxx--xxxxxxx--x--x--x--x--xxxx--xxxxxxx--xxxxxxx",
+"x--x-----x--x--x--x--x--x-----x-----x--x--x--x-----------x--x",
+"x--x--xxxx--x--x--x--x--xxxx--xxxxxxx--x--x--xxxxxxx--xxxx--x",
+"x-----------x--------x-----x--x--------x-----x--x--x--------x",
+"x--xxxxxxxxxxxxx--xxxx--xxxx--x--xxxx--xxxxxxx--x--xxxx--x--x",
+"x-----------x--x-----x--x-----x--x-----x-----x-----------x--x",
+"xxxx--xxxx--x--x--x--x--x--xxxxxxxxxxxxx--x--x--x--xxxxxxxxxx",
+"x--x--x--x-----x--x--x--------x--x--------x-----x--------x--x",
+"x--xxxx--xxxx--xxxx--x--x--x--x--x--xxxx--xxxxxxx--x--xxxx--x",
+"x-----------x--x-----x--x--x--x-----x-----x--------x--------x",
+"xxxx--xxxx--x--x--x--xxxxxxx--x--xxxxxxxxxxxxx--xxxx--xxxx--x",
+"x--------x--------x--x--x--x-----x-----------x--x--x-----x--x",
+"xxxxxxxxxxxxxxxxxxx--x--x--xxxx--xxxx--xxxxxxxxxx--xxxx--x--x",
+"x--------------x--x--------x-----x-----x--x--x--------x--x--x",
+"x--xxxx--xxxxxxx--xxxx--xxxx--x--x--xxxx--x--xxxx--x--x--x--x",
+"x--x-----x-----x--------x-----x--x-----x--x--------x-----x--x",
+"xxxx--x--xxxx--xxxx--x--x--xxxxxxxxxx--x--xxxxxxxxxx--xxxxxxx",
+"x-----x--x-----x--x--x--x--------x--------x-----------------x",
+"x--xxxx--xxxx--x--xxxxxxx--x--xxxx--x--xxxxxxx--xxxx--xxxxxxx",
+"x-----x--------------x-----x--x-----x--x--------x--x--------x",
+"x--x--x--xxxxxxxxxx--x--x--x--xxxxxxx--xxxxxxxxxx--xxxx--xxxx",
+"x--x--x-----x--x--x--x--x--x-----x-----x-----------x-----x--x",
+"x--xxxx--x--x--x--xxxxxxxxxxxxxxxx--x--x--xxxxxxx--x--xxxx--x",
+"x--x-----x-----x--x--------------x--x-----x--x-----x--x-----x",
+"xxxx--xxxxxxx--x--xxxx--xxxx--x--x--xxxxxxx--xxxx--x--x--x--x",
+"x-----x--------------x--x-----x--------------x--x--x-----x--x",
+"x--xxxxxxx--xxxxxxxxxx--xxxx--xxxx--xxxx--xxxx--x--x--x--xxxx",
+"x--x-----------x--x--------x--x--------x--x-----x-----x--x--x",
+"xxxxxxxxxx--xxxx--xxxx--xxxxxxxxxxxxxxxx--x--xxxx--x--xxxx--x",
+"x--------x--------------x--x-----------x--x-----x--x--------x",
+"x--xxxxxxxxxxxxxxxx--x--x--xxxx--x--x--x--x--x--x--xxxxxxxxxx",
+"x--------------------x--------x--x--x--------x--x-----------",
+"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx--",
 ];
 let scene;
 
@@ -22,11 +51,6 @@ window.addEventListener("DOMContentLoaded",function() {
     for(let c = 0; c < cols.length; c++){
       if(cols[c] == "x"){
         new Block(c,1,r)
-      }
-      else if(cols[c] == "T"){
-        new Tree(c,1,r);
-      }else if(cols[c]=="o"){
-        new Rock(c,1,r)
       }
     }
     
